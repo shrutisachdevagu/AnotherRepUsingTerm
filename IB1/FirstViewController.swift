bd6ee8e251b666daf9073edd9868a70c191f0789
Shruti <shruti.sachdeva@galgotiasuniversity.edu.in>
2024-12-08 17:04:15 +0530

    Second Commit

---
+++ IB1/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  IB1
+//
+//  Created by Shruti Sachdeva on 08/12/24.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    // new file added
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
